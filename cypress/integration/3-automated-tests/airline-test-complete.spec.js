9157c9bc43e53deb757c975b0a61b71f459fbe55
angelica.arroyave <angelica.arroyave@landsoft.com.co>
2022-03-15 10:46:13 -0500

    Creación y ejecución exitosa de segunda prueba -> test completo con un pasajero ya registrado en la base de datos

--- cypress/integration/3-automated-tests/airline-test-complete.spec.js
+++ cypress/integration/3-automated-tests/airline-test-complete.spec.js
@@ -1,17 +1,33 @@
 /// <reference types="Cypress" />
  
-it('pagina de inicio', () => {
+it('test completo con pasajero regsitrado', () => {
     cy.visit('http://localhost:4200');
     cy.get('#departureFlight').click();
+    cy.wait(2000);
     cy.get(':nth-child(1) > .form-floating > .form-select').select('Cartagena');
+    cy.wait(2000);
     cy.get(':nth-child(2) > .form-floating > .form-select').select('Bogota');
+    cy.wait(2000);
     cy.get('#btnPassengers').click();
     cy.get('#numAdult').type('1');
+    cy.wait(2000);
     cy.get('#numChild').type('0');
+    cy.wait(2000);
     cy.get('#numInfant').type('0');
+    cy.wait(2000);
     cy.get('.modal-footer > .btn-success').click();
     cy.get(':nth-child(3) > :nth-child(1) > .mb-3 > .form-control').click();
     cy.get(':nth-child(3) > :nth-child(1) > .mb-3 > .form-control').type('2021-11-21');
+    cy.wait(2000);
     cy.get('#btnSearch').click();
     cy.get('#selectFlight').click();
+    cy.get('#document').type('1095251264');
+    cy.wait(2000);
+    cy.get('.docPassenger > .btn').click();
+    cy.wait(1000);
+    cy.get('.modal-footer > .btn').click();
+    cy.wait(1000);
+    cy.get('#btnTotalPayment').click();
+    cy.wait(1000);
+    cy.get('#btnConfirm').click();
 });
