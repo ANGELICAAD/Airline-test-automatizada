503f002f3c1fba7ab0cb632fc2fcf619203a2bdd
angelica.arroyave <angelica.arroyave@landsoft.com.co>
2022-05-08 20:39:17 -0500

    Modificación de url en heroku y actulización de captura de elementos

--- cypress/integration/3-automated-tests/airline-test-flight-list.spec.js
+++ cypress/integration/3-automated-tests/airline-test-flight-list.spec.js
@@ -1,23 +1,27 @@
 /// <reference types="Cypress" />
  
 it('buscar lista de vuelos', () => {
-    cy.visit('http://localhost:4200');
+    // cy.visit('http://localhost:4200');
+    cy.visit('https://angular-airline.herokuapp.com/');
     cy.get('#departureFlight').click();
     cy.wait(2000);
-    cy.get(':nth-child(1) > .form-floating > .form-select').select('Cartagena');
+    cy.get(':nth-child(1) > .form-floating > .form-select').select('Bogota');
     cy.wait(2000);
-    cy.get(':nth-child(2) > .form-floating > .form-select').select('Bogota');
+    cy.get(':nth-child(2) > .form-floating > .form-select').select('Cancun');
     cy.wait(2000);
     cy.get('#btnPassengers').click();
-    cy.get('#numAdult').type('1');
+    // cy.get('#numAdult').type('1');
+    cy.get('.modal-body > .container > form.ng-untouched > :nth-child(1) > .form-control').type('1');
     cy.wait(2000);
-    cy.get('#numChild').type('0');
+    // cy.get('#numChild').type('0');
+    cy.get('form.ng-untouched > :nth-child(2) > .form-control').type('0');
     cy.wait(2000);
-    cy.get('#numInfant').type('0');
+    // cy.get('#numInfant').type('0');
+    cy.get('.ng-invalid.ng-dirty > :nth-child(3) > .form-control').type('0');
     cy.wait(2000);
     cy.get('.modal-footer > .btn-success').click();
     cy.get(':nth-child(3) > :nth-child(1) > .mb-3 > .form-control').click();
-    cy.get(':nth-child(3) > :nth-child(1) > .mb-3 > .form-control').type('2021-11-21');
+    cy.get(':nth-child(3) > :nth-child(1) > .mb-3 > .form-control').type('2022-11-01');
     cy.wait(2000);
     cy.get('#btnSearch').click();
 });
